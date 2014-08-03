9ab5061b7085ad00ecdd029c03d04d0fbf393813
Austin Byrd <austin@byrdfam.org>
2014-08-03 14:11:30 -0700

    rvb

--- hello world.rb
+++ hello world.rb
@@ -1,6 +1,6 @@
-puts "quick grab that thing"
-print "what thing? "
-thing = gets()
-puts "quick grab that#{thing}"
+puts "my name is michel j caboose and i hate"
+print "what does michel j caboose hate"
+random_thing = gets()
+puts "my name is michel j caboose and i hate #{random_thing}"
 
 
